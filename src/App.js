90372b22c7efaac3f804b96ac98d8b8d9c511c41
Kinjal Jagani <kinjalakabari@gmail.com>
2024-03-21 15:18:23 +0530

    addied basic layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 
 
@@ -6,7 +6,19 @@ function App() {
   
   return (
     <div>
-      <div>Name : kinjal</div>
+      <div>Name : Luke Skywalker</div>
+
+      <div className="modal">
+          <div className="modal-content">
+            
+            <h2>Luke Skywalker</h2>
+            <p>Height: 1.64 meters</p>
+            <p>Mass: 50 kg</p>
+            <p>Date added to API: 2014-12-10</p>
+            <p>Films: 4</p>
+            <p>Birth Year: 33BBY</p>
+          </div>
+        </div>
     </div>
   );
   
